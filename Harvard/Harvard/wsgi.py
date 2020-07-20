77bcc6ba56f5f00449fb1cbe4a12ed36fd4c8cf6
Eduardo Menezes Moraes <eduardomenezesm@msn.com>
2020-07-19 21:07:24 -0300

    last classes

---
+++ Harvard/Harvard/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for Harvard project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Harvard.settings')
+
+application = get_wsgi_application()
